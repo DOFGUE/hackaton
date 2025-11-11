df8a689f718bb56fc4e239f6dcbfd46e7f4bc13e
Johann David Niño Ariza <u20241220741@gmail.com>
2025-11-11 13:42:45 -0500

    final final

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
--- Insertar estados iniciales si no existen
-INSERT IGNORE INTO estados (idEstado, nombreEstado) VALUES (1, 'ESPERA');
-INSERT IGNORE INTO estados (idEstado, nombreEstado) VALUES (2, 'EN VALIDACION');
-INSERT IGNORE INTO estados (idEstado, nombreEstado) VALUES (3, 'EN EJECUCION');
-INSERT IGNORE INTO estados (idEstado, nombreEstado) VALUES (4, 'SOLUCIONADO');
-INSERT IGNORE INTO estados (idEstado, nombreEstado) VALUES (5, 'CERRADO');
